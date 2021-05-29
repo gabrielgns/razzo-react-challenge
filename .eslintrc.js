5cfe5f24e8641c326026612f28736a9c3db6c579
gabrielgns <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2021-05-29 20:49:48 -0300

    removing boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['prettier', 'prettier/react', 'plugin:react/recommended', 'airbnb'],
+  extends: ['prettier', 'plugin:react/recommended', 'airbnb'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
@@ -12,11 +12,11 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
     'jsx-quotes': ['error', 'prefer-single'],
   },
-}
+};
