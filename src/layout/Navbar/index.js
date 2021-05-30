1496f7ba8f338f9e8b696c4535a45d68596ddcbf
gabrielgns <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2021-05-30 16:06:16 -0300

    Navbar corrections

--- src/layout/Navbar/index.js
+++ src/layout/Navbar/index.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiShoppingBag } from 'react-icons/bi';
 import { MdKeyboardArrowDown } from 'react-icons/md';
-import razzoLogo from '../../assets/razzoLogo.svg';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+import RazzoLogo from '../../assets/RazzoLogo.svg';
 import {
   NavContainer,
-  IconBg,
-  PageIndicator,
-  LineIndicator,
+  LogoContainer,
+  LogoSvg,
+  CurrentPage,
+  NavText,
+  SelectedPage,
+  IconWrapper,
   UserPhoto,
   NavSeparetor,
 } from './styles';
 
-const Navbar = () => (
-  <NavContainer>
-    <NavSeparetor>
-      <img src={razzoLogo} alt='Logo' />
-      <PageIndicator>
-        Dashboard
-        <LineIndicator />
-      </PageIndicator>
-    </NavSeparetor>
-    <NavSeparetor>
-      <IconBg>
-        <BiShoppingBag />
-      </IconBg>
-      <UserPhoto />
-      <MdKeyboardArrowDown />
-    </NavSeparetor>
-  </NavContainer>
-);
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <NavContainer>
+      <NavSeparetor>
+        <LogoContainer href='/'>
+          <LogoSvg src={RazzoLogo} />
+        </LogoContainer>
+        <GiHamburgerMenu onClick={handleClick} />
+        <CurrentPage>
+          <NavText onClick={closeMobileMenu}>Dashboard</NavText>
+          <SelectedPage />
+        </CurrentPage>
+      </NavSeparetor>
+      <NavSeparetor>
+        <IconWrapper>
+          <BiShoppingBag />
+        </IconWrapper>
+        <UserPhoto />
+        <MdKeyboardArrowDown />
+      </NavSeparetor>
+    </NavContainer>
+  );
+};
 
 export default Navbar;
--- src/layout/Navbar/styles.js
+++ src/layout/Navbar/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import ProfilePicture from '../../assets/profilePicture.jpg';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import ProfilePicture from '../../assets/ProfilePicture.jpg';
 
 export const NavContainer = styled.nav`
   background: #f7f9fc;
@@ -9,40 +10,67 @@ export const NavContainer = styled.nav`
   height: 10vh;
   padding-left: 2rem;
   padding-right: 2rem;
+
+  @media only screen and (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
-export const IconBg = styled.div`
+export const LogoContainer = styled.a`
+  cursor: pointer;
+  display: flex;
+`;
+
+export const LogoSvg = styled.img`
+  width: 100px;
+`;
+
+export const HamburguerMenu = styled(GiHamburgerMenu)`
+  @media only screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const CurrentPage = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  color: #249cf2;
+  font-weight: bold;
+  position: relative;
+  margin-left: 10rem;
+  cursor: pointer;
+`;
+
+export const NavText = styled.p`
+  padding-top: 20%;
+  font-size: 12px;
+`;
+
+export const SelectedPage = styled.div`
+  width: 100%;
+  height: 3%;
+  background: #249cf2;
+`;
+
+export const IconWrapper = styled.div`
   background: #fff;
   border-radius: 50%;
-  height: 6vh;
-  width: 2vw;
+  height: 30px;
+  width: 30px;
   box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-export const PageIndicator = styled.div`
-  color: #249cf2;
-  font-weight: bold;
-  cursor: pointer;
-  position: relative;
-  margin-left: 10rem;
-`;
-
-export const LineIndicator = styled.div`
-  width: 100%;
-  height: 10%;
-  background: #249cf2;
-  position: absolute;
-  margin-top: 1.2rem;
-`;
-
 export const UserPhoto = styled.div`
   background: url(${ProfilePicture}) no-repeat center;
   border-radius: 50%;
-  height: 6vh;
-  width: 2vw;
+  height: 30px;
+  width: 30px;
   box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.1);
   margin-left: 1rem;
 `;
@@ -50,4 +78,5 @@ export const UserPhoto = styled.div`
 export const NavSeparetor = styled.div`
   display: flex;
   align-items: center;
+  height: 100%;
 `;
