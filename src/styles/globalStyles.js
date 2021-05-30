49e805f34d4b42881d966b696c2aac68f6187ebf
gabrielgns <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2021-05-29 21:19:15 -0300

    adding axios

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -2,11 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     body {
+        box-sizing: border-box;
+        height: 100%;
+        width: 100%;
         margin: 0;
         padding: 0;
         background: #fff;
         font-family: Gordita;
-}
+    }
+
+    button {
+      cursor: pointer;
+    }
 `;
 
 export default GlobalStyle;
